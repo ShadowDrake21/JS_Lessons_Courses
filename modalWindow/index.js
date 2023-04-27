ff94c2de55c7eeac0c1e5985e88adca83b27d2d8
Dmytro <122775021+ShadowDrake21@users.noreply.github.com>
2023-04-27 09:42:15 +0300

    Модальное окно JS

---
+++ modalWindow/index.js
@@ -0,0 +1,81 @@
+let fruits = [
+  {
+    id: 1,
+    title: 'Яблоки',
+    price: 20,
+    img: 'https://api.culture.pl/sites/default/files/styles/1920_auto/public/2019-12/jablka_sad_forum-0429287711.jpg?itok=JyMAvt30',
+  },
+  {
+    id: 2,
+    title: 'Апельсины',
+    price: 30,
+    img: 'https://chemwatch.net/wp-content/uploads/2021/11/image-6-1024x682.jpeg',
+  },
+  {
+    id: 3,
+    title: 'Манго',
+    price: 40,
+    img: 'https://images.prom.ua/1289179583_1289179583.jpg?PIMAGE_ID=1289179583',
+  },
+]
+
+const toHTML = (fruit) => `
+<div class="col">
+      <div class="card">
+        <img class="card-img-top" style="height: 300px;" src="${fruit.img}" alt="${fruit.title}">
+        <div class="card-body">
+          <h5 class="card-title">${fruit.title}</h5>
+          <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
+          <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Удалить</a>
+        </div>
+      </div>
+    </div>`
+
+function render() {
+  const html = fruits.map(toHTML).join('')
+  document.querySelector('#fruits').innerHTML = html
+}
+
+render()
+
+const priceModal = $.modal({
+  title: 'Цена на товар',
+  closable: true,
+  width: '400px',
+  footerButtons: [
+    {
+      text: 'Закрыть',
+      type: 'primary',
+      handler() {
+        priceModal.close()
+      },
+    },
+  ],
+})
+
+document.addEventListener('click', (event) => {
+  event.preventDefault()
+  const btnType = event.target.dataset.btn
+  const id = +event.target.dataset.id
+  const fruit = fruits.find((f) => f.id === id)
+
+  if (btnType === 'price') {
+    priceModal.setContent(`
+    <p>Цена на ${fruit.title}: <strong>${fruit.price}$</strong></p>
+    `)
+    priceModal.open()
+    console.log(id, fruit)
+  } else if (btnType === 'remove') {
+    $.confirm({
+      title: 'Вы уверены?',
+      content: `<p>Вы удаляете фрукт: <strong>${fruit.title}</strong></p>`,
+    })
+      .then(() => {
+        fruits = fruits.filter((f) => f.id !== id)
+        render()
+      })
+      .catch(() => {
+        console.log('Cancel')
+      })
+  }
+})
