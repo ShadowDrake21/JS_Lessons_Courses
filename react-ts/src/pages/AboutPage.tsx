25ca1388ce759460d4b1e6d9a67120726a89f6a1
Dmytro <122775021+ShadowDrake21@users.noreply.github.com>
2023-05-14 06:48:12 +0300

    React и TypeScript - Быстрый Курс [2022]

---
+++ react-ts/src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+      dolore libero animi quod voluptas ullam aperiam non numquam veritatis illo
+      eveniet a consequuntur, quas quo ab harum at. Praesentium nobis ut
+      pariatur ipsa rem quis obcaecati maxime consequatur repudiandae
+      exercitationem voluptatibus hic cupiditate, dolores rerum accusamus
+      explicabo officia, magni laudantium.
+    </div>
+  )
+}
